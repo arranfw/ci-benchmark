fdf62a2a57ca3c565b2ad00cac79ab605bfbf51b
github-action-benchmark <github@users.noreply.github.com>
2024-08-23 16:14:36 +0000

    add Benchmark (benchmarkjs) benchmark result for 2f46bb89ea8e5456f64e20e5a6c75ea71f9a1563

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724429559707,
+  "lastUpdate": 1724429676762,
   "repoUrl": "https://github.com/arranfw/ci-benchmark",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,51 @@ window.BENCHMARK_DATA = {
           {
             "name": "fs.readFile()",
             "value": 590,
+            "range": "±0.97%",
+            "unit": "ops/sec",
+            "extra": "74 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arran.woodruff@teamexos.com",
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "committer": {
+            "email": "arran.woodruff@teamexos.com",
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "distinct": true,
+          "id": "2f46bb89ea8e5456f64e20e5a6c75ea71f9a1563",
+          "message": "new data",
+          "timestamp": "2024-08-23T10:13:58-06:00",
+          "tree_id": "7a841afd554a33cb5cef1d17ad7a86246aeebf6c",
+          "url": "https://github.com/arranfw/ci-benchmark/commit/2f46bb89ea8e5456f64e20e5a6c75ea71f9a1563"
+        },
+        "date": 1724429676503,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "deffered.resolve()",
+            "value": 685,
+            "range": "±0.47%",
+            "unit": "ops/sec",
+            "extra": "11 samples"
+          },
+          {
+            "name": "setImmediate()",
+            "value": 695,
+            "range": "±0.23%",
+            "unit": "ops/sec",
+            "extra": "45 samples"
+          },
+          {
+            "name": "fs.readFile()",
+            "value": 690,
             "range": "±0.97%",
             "unit": "ops/sec",
             "extra": "74 samples"

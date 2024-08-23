099451a05ec27c5c0ca435df5335f485302ace34
github-action-benchmark <github@users.noreply.github.com>
2024-08-23 19:56:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0970ae7e8febdc0c14d82afef12c1cefbf00fbcb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,311 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724442839702,
+  "lastUpdate": 1724442961062,
   "repoUrl": "https://github.com/arranfw/ci-benchmark",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "9450aa3ab228ca9c5aa29ccd77f6c46ca600d562",
-          "message": "use custom format, duh",
-          "timestamp": "2024-08-23T10:34:38-06:00",
-          "tree_id": "508c8dbfe2826b74f1079972301234538351b9e7",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/9450aa3ab228ca9c5aa29ccd77f6c46ca600d562"
-        },
-        "date": 1724430895373,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "e2e tests",
-            "value": 520,
-            "unit": "Seconds"
-          },
-          {
-            "name": "unit tests",
-            "value": 80,
-            "unit": "Seconds"
-          },
-          {
-            "name": "build",
-            "value": 35,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "7616264b1d0b735144b7a2e03017ec281ae54f30",
-          "message": "use custom format, duh",
-          "timestamp": "2024-08-23T10:35:40-06:00",
-          "tree_id": "5d5f1071d509fd2bd43e277f68e175aa071b04cc",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/7616264b1d0b735144b7a2e03017ec281ae54f30"
-        },
-        "date": 1724430957299,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "e2e tests",
-            "value": 320,
-            "unit": "Seconds"
-          },
-          {
-            "name": "unit tests",
-            "value": 110,
-            "unit": "Seconds"
-          },
-          {
-            "name": "build",
-            "value": 39,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "e184fae7605cca4879069da6ff2def987d7ef228",
-          "message": "new data",
-          "timestamp": "2024-08-23T10:36:32-06:00",
-          "tree_id": "02c29b2f6fdcbf6ec3e55647c147c5ded37bcf11",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/e184fae7605cca4879069da6ff2def987d7ef228"
-        },
-        "date": 1724431013037,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "e2e tests",
-            "value": 350,
-            "unit": "Seconds"
-          },
-          {
-            "name": "unit tests",
-            "value": 120,
-            "unit": "Seconds"
-          },
-          {
-            "name": "build",
-            "value": 45,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "1b607e09f755d66134e6b8845421f6854752cd6e",
-          "message": "fix duration calculation",
-          "timestamp": "2024-08-23T10:39:26-06:00",
-          "tree_id": "8705f649241cfae02875250b1b0952851c1b7ef5",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/1b607e09f755d66134e6b8845421f6854752cd6e"
-        },
-        "date": 1724431185377,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "e2e tests",
-            "value": 350,
-            "unit": "Seconds"
-          },
-          {
-            "name": "unit tests",
-            "value": 120,
-            "unit": "Seconds"
-          },
-          {
-            "name": "build",
-            "value": 45,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "0a89a84746a075848e878770f32d7ce21f1eb6f7",
-          "message": "fix duration calculation",
-          "timestamp": "2024-08-23T12:56:03-06:00",
-          "tree_id": "9bc8860857695867d79a9bdb34a3598b5f4b1682",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/0a89a84746a075848e878770f32d7ce21f1eb6f7"
-        },
-        "date": 1724439394327,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "e2e tests",
-            "value": 350,
-            "unit": "Seconds"
-          },
-          {
-            "name": "unit tests",
-            "value": 120,
-            "unit": "Seconds"
-          },
-          {
-            "name": "build",
-            "value": 45,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "846588abe757065aaa833428676bae539d6df31e",
-          "message": "bench deploys",
-          "timestamp": "2024-08-23T13:11:58-06:00",
-          "tree_id": "c7a6d5a8bf12c327ecb6392c3bcf1558ee5f4682",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/846588abe757065aaa833428676bae539d6df31e"
-        },
-        "date": 1724440336341,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "deploy",
-            "value": "4",
-            "unit": "seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "email": "arran.woodruff@teamexos.com",
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "distinct": true,
-          "id": "92456e4287b8405cf6f76aebe04565dd3a538df0",
-          "message": "test commit",
-          "timestamp": "2024-08-23T13:12:50-06:00",
-          "tree_id": "c7a6d5a8bf12c327ecb6392c3bcf1558ee5f4682",
-          "url": "https://github.com/arranfw/ci-benchmark/commit/92456e4287b8405cf6f76aebe04565dd3a538df0"
-        },
-        "date": 1724440387601,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "deploy",
-            "value": "4",
-            "unit": "seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "id": "2e6420e39aa03559ccea5c36666dd75badbe06bb",
-          "message": "test branch behaviour",
-          "timestamp": "2024-08-23T19:12:55Z",
-          "url": "https://github.com/arranfw/ci-benchmark/pull/1/commits/2e6420e39aa03559ccea5c36666dd75badbe06bb"
-        },
-        "date": 1724442432041,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "deploy",
-            "value": "4",
-            "unit": "seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "committer": {
-            "name": "arranfw",
-            "username": "arranfw"
-          },
-          "id": "17ca8ea973ad9e6c4036cb44d9c98eccb7219a06",
-          "message": "test branch behaviour",
-          "timestamp": "2024-08-23T19:12:55Z",
-          "url": "https://github.com/arranfw/ci-benchmark/pull/1/commits/17ca8ea973ad9e6c4036cb44d9c98eccb7219a06"
-        },
-        "date": 1724442784704,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "deploy",
-            "value": "4",
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -327,6 +24,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "test",
             "value": "10",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "committer": {
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "id": "0970ae7e8febdc0c14d82afef12c1cefbf00fbcb",
+          "message": "test branch behaviour",
+          "timestamp": "2024-08-23T19:12:55Z",
+          "url": "https://github.com/arranfw/ci-benchmark/pull/1/commits/0970ae7e8febdc0c14d82afef12c1cefbf00fbcb"
+        },
+        "date": 1724442960805,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test",
+            "value": "13",
             "unit": "seconds"
           }
         ]

846cf953a1e05c6dfec688579df814e8816d0aa5
github-action-benchmark <github@users.noreply.github.com>
2024-08-23 19:53:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fcd20362324192e55e43d883434556c0ef43a004

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724442784986,
+  "lastUpdate": 1724442839702,
   "repoUrl": "https://github.com/arranfw/ci-benchmark",
   "entries": {
     "Benchmark": [
@@ -302,6 +302,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "deploy",
             "value": "4",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "committer": {
+            "name": "arranfw",
+            "username": "arranfw"
+          },
+          "id": "fcd20362324192e55e43d883434556c0ef43a004",
+          "message": "test branch behaviour",
+          "timestamp": "2024-08-23T19:12:55Z",
+          "url": "https://github.com/arranfw/ci-benchmark/pull/1/commits/fcd20362324192e55e43d883434556c0ef43a004"
+        },
+        "date": 1724442838818,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test",
+            "value": "10",
             "unit": "seconds"
           }
         ]
